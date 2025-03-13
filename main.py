103cf7e4c1b4542488d1e26468403de69fcd65a5
integralfunction <83551660+integralfunction@users.noreply.github.com>
2025-03-12 23:17:47 -0400

    v2

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-import sys
 import string
-args = sys.argv[1::]
-num_args = len(args)
-if (num_args != 3):
-    print("not allowed")
-    exit()
+import argparse
 
-alphabet = list(string.ascii_uppercase)
-# print(alphabet)
+alphabet: list = list(string.ascii_uppercase)
 
-encrypt = (args[0] == 'e')
-# key = int(args[1])
-# print(key)
-msg = args[2]
 
-def encrypt_alg(msg, key):
+def left_shift(message: str, key: int):
     new = []
-    for idx, msg_char in enumerate(list(msg)):
-        new.append(alphabet[(alphabet.index(msg_char) + key) % 26])
+    for char in list(message):
+        new_index = alphabet.index(char) - key
+        new.append(alphabet[new_index % 26])
     return "".join(new)
 
-def decrypt_alg(msg, key):
+
+def right_shift(message: str, key: int):
     new = []
-    for idx, msg_char in enumerate(list(msg)):
-        new.append(alphabet[(alphabet.index(msg_char) - key) % 26])
+    for char in list(message):
+        new_index = alphabet.index(char) + key
+        new.append(alphabet[new_index % 26])
     return "".join(new)
 
-if (encrypt):
-    encrypted_msg = encrypt_alg(msg, int(args[1]))
-    print(encrypted_msg)
-else:
-    decrypted_msg = decrypt_alg(msg, int(args[1]))
-    print(decrypted_msg)
-    # for key in range(0,27):
-    #     print(f"key: {key} | output: {decrypt_alg(args[2], key)}")
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog='main.py',
+        description='Simple Caesar Cipher. Only supports uppercase!')
+    parser.add_argument('crypt', help='to encrypt or decrypt',
+                        choices=('encrypt', 'decrypt'))
+    parser.add_argument('key', help='how much to shift by', type=int)
+    parser.add_argument('message', type=str)
+    args = parser.parse_args()
+
+    crypt: str = args.crypt
+    key: int = args.key
+    message: str = args.message
+
+    if (not message):
+        print("Message can't be empty")
+        return
+    if (key < 0):
+        print("key can't be negative")
+        return
+    if (crypt == 'encrypt'):
+        print(right_shift(message, key))
+    else:
+        print(left_shift(message, key))
+
+
+if __name__ == '__main__':
+    main()
